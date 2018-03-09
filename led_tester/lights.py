5642c98bb3f19a3354b0c927321f586c79f57d79
Eimear Galligan <eimearni.gh@gmail.com>
2018-03-09 14:06:30 +0000

    Code written to pass grid size test.

--- led_tester/lights.py
+++ led_tester/lights.py
@@ -16,14 +16,36 @@ class LEDTester(object):
         '''
         self.lights = [[False]*N for i in range(N)]
         
-    def apply(self, cmd):
-        if cmd =='switch':
-        elif cmd == 'turn on':
-        elif cmd == 'turn off':
-            
-    def count(self):
+    def size(self):
         count = 0
-        for val in self:
-            if val == "True"
-            count += 1
+        for i in range(0, len(self.lights)):
+            for val in self.lights[i]:
+                count += 1
         return count
+        
+   # def apply(self, cmd):
+    #    if cmd =='switch':  
+     #       print('switch')
+        #elif cmd == 'turn on':
+        #elif cmd == 'turn off':
+        
+       
+    #def count(self):
+     #   count = 0
+      #  for val in self:
+       #     if val == "True":
+        #        print('Found light on')
+         #   count += 1
+        #return count
+    
+   # def turnOn(self):
+    #    for val in self:
+     #       if val == 'False':
+     #           val = ['True']
+      #      print('Light turned on')
+                
+    #def turnOff(self):
+     #   for val in self:
+      #      if val == 'True':
+       #         val = ['False']
+        #    print('Light turned off')
